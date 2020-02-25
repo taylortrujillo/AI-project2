f2b7582d8b2c10681e33adecd7d1dd4565f551e3
taylortrujilo <trujillotaylor48@gmail.com>
2020-02-25 15:27:53 -0700

    initial commit

---
+++ cmake-build-debug/CMakeFiles/projects2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projects2.dir/maxconnect4.cpp.o"
+  "projects2"
+  "projects2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projects2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
